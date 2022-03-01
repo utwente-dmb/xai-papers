d66a6aa991edb30e21dae4702e3b212452940cbe
Boris Gerretzen <borisgerretzen@gmail.com>
2022-03-01 02:31:42 +0100

    Connected flask to mongo

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+import os
+
+# Setup mongo
 app = Flask(__name__)
+print(os.environ)
+app.config['MONGODB_DB'] = 'db'
+app.config['MONGODB_HOST'] = 'mongo'
+app.config['MONGODB_PORT'] = 27017
+app.config['MONGODB_USERNAME'] = os.environ["MONGO_INITDB_ROOT_USERNAME"]
+app.config['MONGODB_PASSWORD'] = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
+db = MongoEngine()
+db.init_app(app)
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  User(name='laura', email='laura@gmail.com').save()
+  return "Python good js bad"
 
 if __name__ == "__main__":
   app.run()
