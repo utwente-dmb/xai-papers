f4cdebfba14200ed0101afccaafcab3db29109ce
GEESE <giesdb@gmail.com>
2022-03-16 12:38:26 +0100

    Removed react-activation again cause I forgot

--- src/pages/Papers.tsx
+++ src/pages/Papers.tsx
@@ -1,21 +1,18 @@
 import React from "react"
-import KeepAlive, { useAliveController } from "react-activation"
 import { Filters, Charts, Papers } from "../components"
 
 function App() {
-	const { refresh } = useAliveController()
 	const [displayPapers, setDisplayPapers] = React.useState<boolean>(true)
 
 	const changeContent = (checked: boolean) => {
 		setDisplayPapers(checked)
-		refresh("papers")
 	}
 
 	return (
-		<KeepAlive name="papers">
+		<>
 			<Filters changeContent={changeContent} />
 			{displayPapers ? <Papers /> : <Charts />}
-		</KeepAlive>
+		</>
 	)
 }
 
