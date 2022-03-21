15f8fdcd3298d263cbe8fe24738a79dfaeefd248
GEESE <giesdb@gmail.com>
2022-03-21 11:58:13 +0100

    Fixed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,27 +1,25 @@
-import "./styles.css";
-import { Card, Col, Row } from "antd";
-import "antd/dist/antd.css";
+import { Card, Col, Row } from "antd"
 
 export default function App() {
-  return (
-    <div className="about">
-      <div className="container">
-        <div className="row align-items-center my-5">
-          <div className="col-lg-7"></div>
-          <h1> Purpose </h1>
-          <p>
+	return (
+		<div className="about">
+			<div className="container"> 
+				<div className="row align-items-center my-5">
+					<div className="col-lg-7"></div>
+					<h1> Purpose </h1>
+					<p>
             The researchers of the Data Management and Biometrics department at
             the University of Twente in collaboration with University of
             Duisburg-Essen conducted a
-            <a href="www.google.com"> large-scale literature review</a> on
+						<a href="www.google.com"> large-scale literature review</a> on
             Explainable Artificial intelligence. This website provides an
             interactive way to explore the dataset and results.
-          </p>
-          <p></p>
+					</p>
+					<p></p>
 
-          <div className="col-lg-5">
-            <h1 className="font-weight-light">Abstract</h1>
-            <p>
+					<div className="col-lg-5">
+						<h1 className="font-weight-light">Abstract</h1>
+						<p>
               The rising popularity of explainable artificial intelligence (XAI)
               to understand high-performing black boxes, also raised the
               question of how to evaluate explanations of machine learning (ML)
@@ -44,36 +42,36 @@ export default function App() {
               opens up opportunities to include quantitative metrics as
               optimization criteria during model training in order to optimize
               for accuracy and interpretability simultaneously.
-            </p>
-          </div>
-          <div className="col-lg-5">
-            <h1 className="font-weight-light">Contributions</h1>
-            <p> Do you want to get involved in the research or have any questions? Please feel free to email us here </p>
+						</p>
+					</div>
+					<div className="col-lg-5">
+						<h1 className="font-weight-light">Contributions</h1>
+						<p> Do you want to get involved in the research or have any questions? Please feel free to email us here </p>
 
-            </div>
+					</div>
 
 
-        </div>
-      </div>
-      <div className="site-card-wrapper">
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="Card title" bordered={true}>
+				</div>
+			</div>
+			<div className="site-card-wrapper">
+				<Row gutter={16}>
+					<Col span={8}>
+						<Card title="Card title" bordered={true}>
               Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={true}>
+						</Card>
+					</Col>
+					<Col span={8}>
+						<Card title="Card title" bordered={true}>
               Get in Touch
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={true}>
+						</Card>
+					</Col>
+					<Col span={8}>
+						<Card title="Card title" bordered={true}>
               Card content
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </div>
-  );
+						</Card>
+					</Col>
+				</Row>
+			</div>
+		</div>
+	)
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,102 +1,104 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Form, Input, InputNumber, Button } from 'antd';
+import React from "react"
+import ReactDOM from "react-dom"
+import "antd/dist/antd.css"
+import "./index.css"
+import { Form, Input, InputNumber, Button } from "antd"
+
+
 const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
+	labelCol: {
+		span: 8,
+	}, 
+	wrapperCol: { 
+		span: 16,
+	},
+}
 /* eslint-disable no-template-curly-in-string */
 
 const validateMessages = {
-  required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
-  },
-  number: {
-    range: '${label} must be between ${min} and ${max}',
-  },
-};
+	required: "${label} is required!",
+	types: {
+		email: "${label} is not a valid email!",
+		number: "${label} is not a valid number!",
+	},
+	number: {
+		range: "${label} must be between ${min} and ${max}",
+	},
+}
 /* eslint-enable no-template-curly-in-string */
 
 const Demo = () => {
-  const onFinish = (values: any) => {
-    console.log(values);
-  };
+	const onFinish = (values: any) => {
+		console.log(values)
+	}
 
-  return (
-    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-      <Form.Item
-        name={['user', 'title']}
-        label="Title"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={['user', 'doi']}
-        label="Doi"
-        rules={[
-          {
-            type: 'email',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={['user', 'age']}
-        label="Year of Publication"
-        rules={[
-          {
-            type: 'number',
+	return (
+		<Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+			<Form.Item
+				name={["user", "title"]}
+				label="Title"
+				rules={[
+					{
+						required: true,
+					},
+				]}
+			>
+				<Input />
+			</Form.Item>
+			<Form.Item
+				name={["user", "doi"]}
+				label="Doi"
+				rules={[
+					{
+						type: "email",
+					},
+				]}
+			>
+				<Input />
+			</Form.Item>
+			<Form.Item
+				name={["user", "age"]}
+				label="Year of Publication"
+				rules={[
+					{
+						type: "number",
             
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Venue">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'Authours']} label="Authours">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Type of Data">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Type of Problem">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Type of Model to be Explained">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Type of Task">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Type of Explanation">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'venue']} label="Method used to explain">
-        <Input />
-      </Form.Item>
-      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
+					},
+				]}
+			>
+				<InputNumber />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Venue">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "Authours"]} label="Authours">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Type of Data">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Type of Problem">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Type of Model to be Explained">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Type of Task">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Type of Explanation">
+				<Input />
+			</Form.Item>
+			<Form.Item name={["user", "venue"]} label="Method used to explain">
+				<Input />
+			</Form.Item>
+			<Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+				<Button type="primary" htmlType="submit">
           Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  );
-};
+				</Button>
+			</Form.Item>
+		</Form>
+	)
+}
 
-ReactDOM.render(<Demo />, document.getElementById('container'));
+ReactDOM.render(<Demo />, document.getElementById("container"))
