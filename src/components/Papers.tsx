3f83f45bfb6c14db64f162851141e39b862335c0
GEESE <giesdb@gmail.com>
2022-03-23 14:29:53 +0100

    Moved expand All button into table header

--- src/components/Papers.tsx
+++ src/components/Papers.tsx
@@ -1,14 +1,15 @@
 import { Table, Row, Button } from "antd"
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons"
 import { useExpandingAllInTable, useFilteredPapers } from "../hooks"
 import {TagList} from "../components"
 import { Paper } from "../types"
 import { getColor, typeArray, printNames } from "../utils"
 
 type Column<T> = {
-	title: string, 
-	dataIndex: string, 
+	title: string | JSX.Element, 
+	dataIndex?: string, 
 	render?: (text: string, row: T) => React.ReactNode, 
-	key: string, 
+	key?: string, 
 	sorter?: {
 		compare: (a: T, b: T) => number,
 		multiple: number
@@ -16,45 +17,7 @@ type Column<T> = {
 	defaultSortOrder?: "ascend" | "descend"
 }
 
-const columns: Array<Column<Paper>> = [
-	{
-		title: "Title",
-		dataIndex: "Title",
-		render: (text: string, row: Paper) => <a href={row.url} target="_blank" rel="noreferrer">{text}</a>,
-		key: "title",
-		defaultSortOrder: "ascend",
-		sorter: {
-			compare: (a: Paper, b: Paper) => a.Title.localeCompare(b.Title),
-			multiple: 1
-		}
-	},
-	{
-		title: "Venue",
-		dataIndex: "Venue",
-		render: (_, row: Paper) => <>{row.Venue.value}</>,
-		key: "venue",
-		defaultSortOrder: "ascend",
-		sorter: {
-			compare: (a: Paper, b: Paper) => a.Venue.value.localeCompare(b.Venue.value),
-			multiple: 2
-		}
-	},
-	{
-		title: "Year",
-		dataIndex: "Year",
-		key: "year",
-		defaultSortOrder: "ascend",
-		sorter: {
-			compare: (a: Paper, b: Paper) => a.Year.localeCompare(b.Year),
-			multiple: 3
-		}
-	},
-	{
-		title: "Author",
-		key: "author",
-		dataIndex: "Author",
-	},
-]
+
 
 function Tag({ record, type }: { record: Paper, type: keyof Paper}) {
 	return (
@@ -75,11 +38,54 @@ function Papers(): JSX.Element {
 	const allKeys = papersData.map((paper) => paper.key)
 	const expandController = useExpandingAllInTable(allKeys, "key", false)
 
+	const columns: Array<Column<Paper>> = [
+		{
+			title: 
+			<Button className="expand-all">
+				{expandController.isAllExpanded() ? <MinusOutlined/> : <PlusOutlined />}
+			</Button>
+		},
+		{
+			title: "Title",
+			dataIndex: "Title",
+			render: (text: string, row: Paper) => <a href={row.url} target="_blank" rel="noreferrer">{text}</a>,
+			key: "title",
+			defaultSortOrder: "ascend",
+			sorter: {
+				compare: (a: Paper, b: Paper) => a.Title.localeCompare(b.Title),
+				multiple: 1
+			}
+		},
+		{
+			title: "Venue",
+			dataIndex: "Venue",
+			render: (_, row: Paper) => <>{row.Venue.value}</>,
+			key: "venue",
+			defaultSortOrder: "ascend",
+			sorter: {
+				compare: (a: Paper, b: Paper) => a.Venue.value.localeCompare(b.Venue.value),
+				multiple: 2
+			}
+		},
+		{
+			title: "Year",
+			dataIndex: "Year",
+			key: "year",
+			defaultSortOrder: "ascend",
+			sorter: {
+				compare: (a: Paper, b: Paper) => a.Year.localeCompare(b.Year),
+				multiple: 3
+			}
+		},
+		{
+			title: "Author",
+			key: "author",
+			dataIndex: "Author",
+		},
+	]
+
 	return (
 		<>
-			<Button className="expand-all">
-				Expand All Rows
-			</Button>
 			<Table
 				style={{ marginTop: 10 }}
 				columns={columns}
@@ -105,6 +111,10 @@ function Papers(): JSX.Element {
 				expandedRowKeys={expandController.expandedRowKeys}
 				onExpand={expandController.onExpand}
 				rowKey={expandController.rowKey}
+				pagination={{
+					defaultPageSize: 25, 
+					position: ["bottomCenter"]
+				}}
 			></Table>
 		</>
 	)
