a8d755a8469e401011df7a7ac55632f9193563a8
GEESE <giesdb@gmail.com>
2022-04-17 20:07:49 +0200

    Renamed chart folder to charts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Filters } from "./Filters"
 export { default as TagList } from "./TagList"
 export { default as Select } from "./Select"
-export * from "./chart"
+export * from "./charts"
