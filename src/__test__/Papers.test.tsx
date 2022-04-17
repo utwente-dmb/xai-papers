36474fba93a80d7c82a090df3a65990ad1652bbb
GEESE <giesdb@gmail.com>
2022-04-17 16:13:08 +0200

    Updated papers test

--- src/__test__/Papers.test.tsx
+++ src/__test__/Papers.test.tsx
@@ -2,12 +2,10 @@ import { render, unmountComponentAtNode } from "react-dom"
 import { act } from "react-dom/test-utils"
 import { Papers } from "../pages"
 import { Provider } from "react-redux"
-import { papersActions, store } from "../redux"
+import { store } from "../redux"
 import "@testing-library/jest-dom"
-import { prettyDOM } from "@testing-library/react"
-import { Data, Explanation, Problem, Model, Task, Method } from "../types/paper"
-import { brotliCompressSync } from "zlib"
-import { useFilteredPapers } from "../hooks"
+import { Data } from "../types/paper"
+
 
 let container: HTMLElement | null = null
 
@@ -30,7 +28,7 @@ describe("Test", () => {
 	it("Checks filtered papers for tags", () => {
 		// Render filters
 		act(() => {
-			render(<Provider store={store}><Papers /></Provider>, container)
+			render(<Provider store={store}> <Papers/> </Provider>, container)
 		})
 
 		// Get all the dropdowns and click them
